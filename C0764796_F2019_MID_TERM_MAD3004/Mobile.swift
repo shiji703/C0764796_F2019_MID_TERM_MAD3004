ff9a4bf868d79894c48772457df6e162427a6453
gurdeep <iamgurdeepbanga@gmail.com>
2019-10-19 18:07:46 -0400

    class mobile

---
+++ C0764796_F2019_MID_TERM_MAD3004/Mobile.swift
@@ -0,0 +1,14 @@
+//
+//  Mobile.swift
+//  C0764796_F2019_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-10-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Mobile : Bill
+{
+    
+}
