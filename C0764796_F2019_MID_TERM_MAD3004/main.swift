654c87eaa9cfb10063e85c3cff8c17b926d312c9
gurdeep <iamgurdeepbanga@gmail.com>
2019-10-19 20:07:00 -0400

    changes

--- C0764796_F2019_MID_TERM_MAD3004/main.swift
+++ C0764796_F2019_MID_TERM_MAD3004/main.swift
@@ -17,7 +17,7 @@ var i1 = Internet(pName: "Rogers", iGbused: 500, bId: 2, bDate: date, bType: "In
 var m = Mobile(mName: "Samsung Galaxy.Inc", planName: "Prepaid Talk", mNumber: 1234567890, iGbused: 5, minutes: 65, bId: 1, bDate: date, bType: "Hydro", bAmount: 300.78)
 var m1 = Mobile(mName: "Apple.Inc", planName: "Prepaid Talk", mNumber: 9876543210, iGbused: 4, minutes: 120, bId: 2, bDate: date, bType: "mobile", bAmount: 45.95)
 var c = Customer(custId: 1, fName: "Shiji", lName: "Eugin", fullName: "Shiji Eugin", eId: "shijieugin@gmailcom")
-
+var c1 = Customer(custId: 2, fName: "Diya", lName: "Thomas", fullName: "Diya Thomas", eId: "diyathomas@gmail.com")
 
 
 
