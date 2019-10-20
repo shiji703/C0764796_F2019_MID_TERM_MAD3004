d89fe4fe60217b9a105099d788ad2a7e09021d9e
gurdeep <iamgurdeepbanga@gmail.com>
2019-10-19 20:05:49 -0400

    add customer values

--- C0764796_F2019_MID_TERM_MAD3004/main.swift
+++ C0764796_F2019_MID_TERM_MAD3004/main.swift
@@ -16,6 +16,9 @@ var i = Internet(pName: "Rogers", iGbused: 500, bId: 1, bDate: date, bType: "Hyd
 var i1 = Internet(pName: "Rogers", iGbused: 500, bId: 2, bDate: date, bType: "Internet", bAmount: 250.69)
 var m = Mobile(mName: "Samsung Galaxy.Inc", planName: "Prepaid Talk", mNumber: 1234567890, iGbused: 5, minutes: 65, bId: 1, bDate: date, bType: "Hydro", bAmount: 300.78)
 var m1 = Mobile(mName: "Apple.Inc", planName: "Prepaid Talk", mNumber: 9876543210, iGbused: 4, minutes: 120, bId: 2, bDate: date, bType: "mobile", bAmount: 45.95)
-var c = Customer(custId: <#T##Int#>, fName: <#T##String#>, lName: <#T##String#>, fullName: <#T##String#>, eId: <#T##String#>)
+var c = Customer(custId: 1, fName: "Shiji", lName: "Eugin", fullName: "Shiji Eugin", eId: "shijieugin@gmailcom")
+
+
+
 
 
